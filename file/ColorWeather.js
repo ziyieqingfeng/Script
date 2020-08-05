0ffbf7767d1a4b2547154989fad77e0f4ceaf6df
abner <ziyieqingfeng@users.noreply.github.com>
2020-08-05 21:10:23 +0800

    Update ColorWeather.js

--- file/ColorWeather.js
+++ file/ColorWeather.js
@@ -1,9 +1,7 @@
 /*
 CaiYun Weather (ColorWeather) unlock Vip
-
 QX 1.0.0:
 ^https:\/\/biz\.caiyunapp\.com\/v2\/user\?app_name\=weather url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/ColorWeather.js
-
 MitM = biz.caiyunapp.com
 */
 
